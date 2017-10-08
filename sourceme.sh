bb08949b5b9939464373471e07409c9476fd77ce
danhvo <danhvo.uit@gmail.com>
2017-10-08 14:49:27 +0700

    Move sourceme.sh into MusicRecommendationSystem dir.
       - Move sourceme.sh and make it to be a source environment variable of whole project.
       - Add .pyc to .gitignore.

--- sourceme.sh
+++ sourceme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CONDA_DIR=$(readlink -f ~/Softwares/miniconda3/bin/)
-export THESIS_HOME=$(readlink -f ../../)
+export THESIS_HOME=$(readlink -f ./)
 export DATA_ARCHIVE=${THESIS_HOME}/DataArchive/
 export CRAWLER_DIR=${THESIS_HOME}/SourceCode/Helpers/DataCrawler/
 export EVENTGEN_DIR=${THESIS_HOME}/SourceCode/Helpers/EventsGenerator/
