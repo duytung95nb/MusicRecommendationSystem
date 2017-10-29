598a0e3759988fac4c74f3edeea0e9cd760d81c8
danhvo <danhvo.uit@gmail.com>
2017-10-29 12:49:24 +0700

    Modify UserEventMaker.py to adapt with fake_user_maker.py
       - Make sure users are generated within user.csv
       - Refactor code in UserEventMaker.py

--- SourceCode/Helpers/EventsGenerator/UserEventMaker.py
+++ SourceCode/Helpers/EventsGenerator/UserEventMaker.py
@@ -8,41 +8,65 @@ import random
 import os
 from random import randint
 
-dataArchivePath = os.getenv("DATA_ARCHIVE")
-userEventPath = "%s/user_event.csv" % dataArchivePath
-metadataPath = "%s/metadata.csv" % dataArchivePath
+class UserEventMaker:
+    def __init__(self):
+        self.dataArchivePath = os.getenv("DATA_ARCHIVE")
+        self.userDataPath = os.path.join(self.dataArchivePath, "user.csv")
+        self.metadataPath = os.path.join(self.dataArchivePath, "metadata.csv")
+        self.userEventPath = os.path.join(self.dataArchivePath, "user_event.csv")
+        self.songIdList = UserEventMaker.get_id_list_from_csv(self.metadataPath, "id")
+        self.userIdList = UserEventMaker.get_id_list_from_csv(self.userDataPath, "user_id")
 
-def getSongIDList(_pathCSVFile):
-    songIDList = []
+    @staticmethod
+    def get_id_list_from_csv(pathFile, title):
+        result = []
 
-    with open(_pathCSVFile) as metadata:
-        csvReader = csv.DictReader(metadata)
-        for row in csvReader:
-            song_id = row["id"]
-            songIDList.append(song_id)
+        with open(pathFile) as metadata:
+            csvReader = csv.DictReader(metadata)
+            for row in csvReader:
+                song_id = row[title]
+                result.append(song_id)
 
-    return songIDList
+        return result
+
+    def validate(self, numberOfUsers, numberOfMusics):
+        if numberOfMusics > len(self.songIdList):
+            print "Number of musics is greater than number of musics in sys"
+            return False
+
+        if numberOfUsers > len(self.userIdList):
+            print "Number of users is greater than number of users in sys"
+            return False
+
+        return True
+
+    def gen(self, numberOfUsers, numberOfMusics):
+        songIdList = UserEventMaker.get_id_list_from_csv(self.metadataPath, "id")
+        userIdList = UserEventMaker.get_id_list_from_csv(self.userDataPath, "user_id")
+
+        with open(self.userEventPath, 'wb') as csvFile:
+            csvWriter = csv.writer(csvFile)
+            csvWriter.writerow(["user_id", "song_id", "rating"])
+
+            for _ in range(int(numberOfUsers)):
+                userID = userIdList.pop(0)
+                randomSongIndexes = random.sample(range(0, len(songIdList) - 1),
+                                                  int(numberOfMusics))
+
+                for index in randomSongIndexes:
+                    randomSongID = songIdList[index]
+                    randomRating = randint(1, 5)
+
+                    data = [userID, randomSongID, randomRating]
+                    csvWriter.writerow(data)
 
 
-# Main function
-numberOfUsers = raw_input("Number of users: ")
-numberOfMusics  = raw_input("Number of musics (each user): ")
-
-songIDList = getSongIDList(metadataPath)
-
-with open(userEventPath, 'wb') as csvfile:
-    csvWriter = csv.writer(csvfile)
-    csvWriter.writerow(["user_id", "song_id", "rating"])
-
-    for userIndex in range(int(numberOfUsers)):
-        userID = "usr" + str(userIndex + 1).zfill(8)
-        randomSongIndexes = random.sample(range(0, len(songIDList) - 1), int(numberOfMusics))
-
-        for index in randomSongIndexes:
-            randomSongID = songIDList[index]
-            randomRating = randint(1, 5)
-
-            data = [userID, randomSongID, randomRating]
-            csvWriter.writerow(data)
-
-print "Saved in: %s" % userEventPath
+if __name__ == '__main__':
+    numberOfUsers = raw_input("Number of users: ")
+    numberOfMusics = raw_input("Number of musics (each user): ")
+    userEventMaker = UserEventMaker()
+    if userEventMaker.validate(int(numberOfUsers), int(numberOfMusics)):
+        userEventMaker.gen(int(numberOfUsers), int(numberOfMusics))
+        print "Succeed"
+    else:
+        print "Failed"
