e25afd3659ef93c86d57f9ed50970d088b4e2e54
duytung95nb <duytung95nb@gmail.com>
2017-11-12 15:21:30 +0700

    Change timestamp data type: uuid to timestamp

--- SourceCode/Helpers/FakeUserGenerator/UserGeneratorBaseOnDatabase/Models/UserEvent.cs
+++ SourceCode/Helpers/FakeUserGenerator/UserGeneratorBaseOnDatabase/Models/UserEvent.cs
@@ -6,12 +6,12 @@ namespace UserGeneratorBaseOnDatabase.Models
     public class UserEvent
     {
         public string UserId { get; set; }
-        public Guid Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
         public string SongId { get; set; }
         public string ActionType { get; set; }
         public string PayLoad { get; set; }
         public UserEvent() {
-            this.Timestamp = Guid.NewGuid();
+            this.Timestamp = DateTime.Now;
         }
         public UserEvent(string _userId, string _songId,
         string _payload, string _actionType)
