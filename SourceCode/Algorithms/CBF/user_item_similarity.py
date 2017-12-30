d6f536cc8c9697831d9af9cf90caff6840b5f661
danhvo <danhvo.uit@gmail.com>
2017-12-30 11:31:52 +0700

    Complete user-item similarity

--- SourceCode/Algorithms/CBF/user_item_similarity.py
+++ SourceCode/Algorithms/CBF/user_item_similarity.py
@@ -1,19 +1,50 @@
 from cassandra.cluster import Cluster
+from pyspark import SparkConf
 from sklearn.metrics.pairwise import cosine_similarity
+from pyspark_cassandra import CassandraSparkContext
+
+
+def similar(user, items, events):
+    top_sim = list()
+    listened_songs = events.get(user[0])
+    for sid, profile in items.items():
+        if sid in listened_songs:
+            continue
+
+        similarity = cosine_similarity([user[1], profile])[0][1]
+        top_sim.append((sid, similarity))
+
+    top_sim.sort(key=lambda tup: tup[1], reverse=True)
+    top_sim = top_sim[:10]
+    top_sim = [sim[0] for sim in top_sim]
+
+    return tuple([user[0], top_sim])
 
 
 class UserItemSimilarity(object):
     def __init__(self):
+        self.spark_config = SparkConf() \
+            .setMaster("local[4]") \
+            .setAppName("ContentBased") \
+            .set("spark.cassandra.connection.host", "127.0.0.1")
+        self.sparkContext = CassandraSparkContext(conf=self.spark_config)
+
         self.cluster = Cluster()
         self.session = self.cluster.connect("music_recommendation")
 
         cql_cmd = "SELECT * FROM %s"
-        self.i_artists = self.session.execute(cql_cmd % "i_profile_artist")
-        self.i_composers = self.session.execute(cql_cmd % "i_profile_composer")
-        self.i_genres = self.session.execute(cql_cmd % "i_profile_genre")
-        self.u_artists = self.session.execute(cql_cmd % "u_profile_artist")
-        self.u_composers = self.session.execute(cql_cmd % "u_profile_composer")
-        self.u_genres = self.session.execute(cql_cmd % "u_profile_genre")
+        cmd = cql_cmd % "i_profile_artist"
+        self.i_artists_res = self.session.execute(cmd)
+        cmd = cql_cmd % "i_profile_composer"
+        self.i_composers_res = self.session.execute(cmd)
+        cmd = cql_cmd % "i_profile_genre"
+        self.i_genres_res = self.session.execute(cmd)
+        cmd = cql_cmd % "u_profile_artist"
+        self.u_artists_res = self.session.execute(cmd)
+        cmd = cql_cmd % "u_profile_composer"
+        self.u_composers_res = self.session.execute(cmd)
+        cmd = cql_cmd % "u_profile_genre"
+        self.u_genres_res = self.session.execute(cmd)
 
         cql_cmd = "SELECT uid, song_id FROM %s"
         events = self.session.execute(cql_cmd % "user_event")
@@ -25,33 +56,77 @@ class UserItemSimilarity(object):
             else:
                 self.events[event.uid].append(event.song_id)
 
+        self.session.execute("CREATE TABLE IF NOT EXISTS "
+                             "result_cb_user_item_genre ("
+                             "uid text PRIMARY KEY,"
+                             "recommendations list<text>);")
+        self.session.execute("CREATE TABLE IF NOT EXISTS "
+                             "result_cb_user_item_artist ("
+                             "uid text PRIMARY KEY,"
+                             "recommendations list<text>);")
+        self.session.execute("CREATE TABLE IF NOT EXISTS "
+                             "result_cb_user_item_composer ("
+                             "uid text PRIMARY KEY,"
+                             "recommendations list<text>);")
+
+    @staticmethod
+    def convert_i(rows):
+        result = dict()
+        for row in rows:
+            result[row.sid] = row.profile
+
+        return result
+
+    @staticmethod
+    def convert_u(rows):
+        result = list()
+        for row in rows:
+            new_tuple = (row.uid, row.profile)
+            result.append(new_tuple)
+
+        return result
+
     def similar_to(self, user, items):
         top_sim = list()
-        listened_songs = self.events.get(user.uid)
-        for item in items:
-            if item.sid in listened_songs:
+        listened_songs = self.events.get(user[0])
+        for sid, profile in items.items():
+            if sid in listened_songs:
                 continue
-            similarity = cosine_similarity([user.profile, item.profile])[0][1]
-            if similarity == 0:
-                print "user_id: %s" % user.uid
-                print "user_profile: %s" % user.profile
-                print "item_id: %s" % item.sid
-                print "item_profile: %s" % item.profile
-                print similarity
-                print "============================"
-            top_sim.append((user.uid, item.sid, similarity))
-            top_sim.sort(key=lambda tup: tup[2])
-            top_sim = top_sim[:10]
+
+            similarity = cosine_similarity([user[1], profile])[0][1]
+            top_sim.append((sid, similarity))
+
+        top_sim.sort(key=lambda tup: tup[1], reverse=True)
+        top_sim = top_sim[:10]
+        top_sim = [sim[0] for sim in top_sim]
 
         print top_sim
 
     def build(self):
-        for u_genre in self.u_genres:
-            self.similar_to(u_genre, self.i_genres)
-        for u_artist in self.u_artists:
-            self.similar_to(u_artist, self.i_artists)
-        for u_composer in self.u_composers:
-            self.similar_to(u_composer, self.i_composers)
+        events = self.events
+
+        i_genres = self.convert_i(self.i_genres_res)
+        i_artists = self.convert_i(self.i_artists_res)
+        i_composers = self.convert_i(self.i_composers_res)
+
+        u_genres = self.convert_u(self.u_genres_res)
+        u_artists = self.convert_u(self.u_artists_res)
+        u_composers = self.convert_u(self.u_composers_res)
+
+        dist_u_genres = self.sparkContext.parallelize(u_genres)
+        dist_u_genres = dist_u_genres.map(lambda u: similar(u, i_genres, events))
+        dist_u_genres.saveToCassandra("music_recommendation",
+                                      "result_cb_user_item_genre")
+
+        dist_u_artists = self.sparkContext.parallelize(u_artists)
+        dist_u_artists = dist_u_artists.map(lambda u: similar(u, i_artists, events))
+        dist_u_artists.saveToCassandra("music_recommendation",
+                                       "result_cb_user_item_artist")
+
+        dist_u_composer = self.sparkContext.parallelize(u_composers)
+        dist_u_composer = dist_u_composer.map(lambda u: similar(u, i_composers, events))
+        dist_u_composer.saveToCassandra("music_recommendation",
+                                        "result_cb_user_item_composer")
 
 
 if __name__ == '__main__':
--- daily-call.sh
+++ daily-call.sh
@@ -2,6 +2,7 @@
 
 THESIS_HOME="/root/project/MusicRecommendationSystem/"
 CF_DIR=${THESIS_HOME}/SourceCode/Algorithms/CF/
+CBF_DIR=${THESIS_HOME}/SourceCode/Algorithms/CBF/
 POPULAR_DIR=${THESIS_HOME}/SourceCode/Algorithms/Popularity/
 
 source activate music-recommendation
@@ -12,6 +13,16 @@ spark-submit --packages anguenot/pyspark-cassandra:0.7.0 main.py
 
 echo "Calculating CF..."
 cd $CF_DIR
+spark-submit --packages anguenot/pyspark-cassandra:0.7.0 main.py
+
+echo "Calculating lyrics similarity..."
+cd $CBF_DIR
+python item_item_similarity.py
+
+echo "Calculating user profile..."
+python build_user_profile.py
+
+echo "Calculating CBF..."
 spark-submit --packages anguenot/pyspark-cassandra:0.7.0 main.py
 
 source deactivate
